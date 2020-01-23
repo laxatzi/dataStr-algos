85387c4cd841b198a54d7e0d6eadb48c59a5a8e4
laxatzi <laxatzi@yahoo.com>
2020-01-23 13:33:30 +0200

    initiate code in test.html and spreadSyntax .js files

--- spreadSyntax.js
+++ spreadSyntax.js
@@ -1,1 +1,10 @@
+function copyMachine(arr, num){
+   let newArr = [];
+   // iteration
+   while(num >= 1) {
+      //code goes here
+   }
+   return newArr;
+}
 
+//console.log(copyMachine([true, false, 1, 0]), 3);
