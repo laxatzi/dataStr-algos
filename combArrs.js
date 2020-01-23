a2fc5022eca63ee7d75cc4c22f97324629cee57a
laxatzi <laxatzi@yahoo.com>
2020-01-23 16:28:43 +0200

    combine arrs with spread operator

--- combArrs.js
+++ combArrs.js
@@ -1,1 +1,11 @@
+//Modify the function using the 'spread op' so that it returns the array ['learning', 'to', 'code', 'is', 'fun'];
 
+function spreadOut(){
+   let fragment = ['to','code'];
+   let sentence = [...fragment];
+   sentence.push('is','fun');
+   sentence.unshift('learning');
+   return sentence;
+}
+
+ console.log(spreadOut());
