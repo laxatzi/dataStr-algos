9da8310b995e7239d9ed0cffc5557049026cb72b
laxatzi <laxatzi@yahoo.com>
2020-01-23 16:41:24 +0200

    check elements precense in an array with indexOf

--- checkPresence.js
+++ checkPresence.js
@@ -1,1 +1,11 @@
+//We have defined a function, 'quickCheck', that takes an array and an element as arguments. Modify the function using 'indexOf()' so that it return 'true' if the passed element exists in the array, and 'false' if not.
 
+function quickCheck(arr, el){
+   if(arr.indexOf(el) == -1){
+      return false;
+   }else{
+      return true;
+   }
+}
+
+// console.log(quickCheck(['onions', 'leeks', 'lentils'], 'mushrooms'));
