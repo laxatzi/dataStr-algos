8a50ea7b64482dd653980e43e5032181ccf950f9
laxatzi <laxatzi@yahoo.com>
2020-04-22 15:49:24 +0300

    create reverseStr.js file

--- longestWord-BAlg.js
+++ longestWord-BAlg.js
@@ -1,11 +1,11 @@
 function findLongestWordLength(str) {
-   //return str.length;
-   const words = str.split(" ");
+  //return str.length;
+  const words = str.split(" ");
 
-   const lengthOfMaxWord = words.map(word=> word.length ); // end of map
-   
-   return Math.max(...lengthOfMaxWord);
+  const lengthOfMaxWord = words.map(word=> word.length ); // end of map
+  
+  return Math.max(...lengthOfMaxWord);
 
- } // end of func
- 
- console.log(findLongestWordLength("Hi my name is Lampros"));
+} // end of func
+
+console.log(findLongestWordLength("Hi my name is Lampros"));
