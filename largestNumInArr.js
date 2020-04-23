76cfc0d95e9634f095b4b7832f948e85ecac79e7
laxatzi <laxatzi@yahoo.com>
2020-04-23 16:39:07 +0300

    update largestNumInArr.js and test.html files

--- largestNumInArr.js
+++ largestNumInArr.js
@@ -1,1 +1,8 @@
 
+function largestOfFour(arr) {
+   const largestNumInArr = arr.map(num=> num.length ); 
+   
+   
+ }
+ 
+ console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
