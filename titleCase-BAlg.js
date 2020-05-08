bd2ccf63d5d8f9f29961dfd1262f5c639c2a3fee
laxatzi <laxatzi@yahoo.com>
2020-05-08 14:06:03 +0300

    resolve titleCase exercise with alternative solution

--- titleCase-BAlg.js
+++ titleCase-BAlg.js
@@ -12,11 +12,13 @@ function titleCase(str) {
     return capitalize.join(' ');
  }
  
- console.log(titleCase("Τι κάνετε όλα καλά;")); //Τι Κάνετε Όλα Καλά;
+ console.log(titleCase("Τι κάνετε, όλα καλά;")); //Τι Κάνετε, Όλα Καλά;
 
 }// end of block
 
 // alternative, cleaner solution using the replace method
+  // The replace method returns a new string with some or all matches of a pattern replaced by a replacement.
+  // The pattern can be a string or a regex, and the replacement can be a string or a function to be called for each match. It pattern is a string, only the first occurrence will be replaced.
 {
    function titleCase(str){
       const convertToArr = str.toLowerCase().split(' ');
@@ -25,7 +27,15 @@ function titleCase(str) {
       });
       return capitalize.join(' ');
    }
-   console.log(titleCase("Τι κάνετε όλα καλά;")); //Τι Κάνετε Όλα Καλά;
+   console.log(titleCase("Τι κάνετε, όλα καλά;")); //Τι Κάνετε, Όλα Καλά;
 
 }// end of block
 
+// alternative, best solution
+{
+   function titleCase(str){
+      return str.toLowerCase().replace(/(^|\s)\S/g, firstChar => firstChar.toUpperCase());
+   }
+
+   console.log(titleCase("Τι κάνετε, όλα καλά;")); //Τι Κάνετε, Όλα Καλά;
+} // end block
