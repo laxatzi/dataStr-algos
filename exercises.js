a8d28dc60077303d85bf5e5d6c8d8f021a8a5a6b
laxatzi <laxatzi@yahoo.com>
2020-05-12 15:56:13 +0300

    update exercises.js file

--- exercises.js
+++ exercises.js
@@ -1,1 +1,25 @@
+// You are given two arrays and an index.
 
+// Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+
+// Begin inserting elements at index n of the second array.
+
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+{
+   function frankenSplice(arr1, arr2, n) {
+      let newArr = arr2.slice();
+      newArr.splice(n, 0, ...arr1);
+      return newArr;
+    }
+    
+  console.log(frankenSplice([1, 2, 3], [4, 5, 6], 0));   // 
+}// end block
+
+{
+   const months = ['Jan', 'March', 'April', 'June'];
+   let someMonths = months.slice(1,3);
+   // console.log('new months: ' + someMonths);
+   // console.log('old monts: '+months);
+
+}
