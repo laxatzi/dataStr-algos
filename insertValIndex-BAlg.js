a0730aba39622545f79b9edd42c0fa769e00511c
laxatzi <laxatzi@yahoo.com>
2020-05-14 11:19:01 +0300

    resolve inserted values index founder challenge"

--- insertValIndex-BAlg.js
+++ insertValIndex-BAlg.js
@@ -1,1 +1,18 @@
-
+//.Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+{
+function getIndexToIns(arr, num) {
+   const sortedArr = arr.sort(function(a,b){
+      return a - b;
+   });
+ 
+   const greaterPart = sortedArr.filter(elem => {
+      return elem > num;
+   });
+   const lowerPart = sortedArr.filter(elem => {
+      return elem < num;
+   });
+   const completePart = lowerPart.concat(num, greaterPart);
+   return completePart.indexOf(num);
+}
+ console.log(getIndexToIns([10, 20, 30, 40, 50], 35)); //should return 3.
+}// end block
