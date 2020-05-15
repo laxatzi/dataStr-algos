b36cfadb33e1d1c2334be578fccb423982003ee3
laxatzi <laxatzi@yahoo.com>
2020-05-15 16:53:48 +0300

    create mutation.js file and set exercise for mutation function in exercises.js file

--- exercises.js
+++ exercises.js
@@ -179,6 +179,24 @@ console.log(titleCase('sHoRt AnD sToUt')); // Short And Stout
 }//block
 
 //EXERCISE #15
+  //.Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+  //.For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+  //.The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+  //.Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+  //? check JavaScript String.prototype.indexOf() 
+
+  {
+   function mutation(arr) {
+     return arr;
+    }
+  
+    //test
+    console.log(mutation(["voodoo", "no"])); //false
+    console.log(mutation(["Mary", "Army"])); //true
+  }//block
 
 //EXERCISE #16
 
