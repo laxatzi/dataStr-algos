cd0b7c95410ce4ab7b5adc36608fa55d5cfbe083
laxatzi <laxatzi@yahoo.com>
2020-05-15 17:08:41 +0300

    set comment on chunky monkey algorithm

--- chunkyMonkey-BAlg.js
+++ chunkyMonkey-BAlg.js
@@ -1,1 +1,19 @@
+//.Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+{
+   function chunkArrayInGroups(arr, size) {
+      return 'original';
+    }
+
+    //test
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)); //should return [[0, 1], [2, 3], [4, 5]].
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)); //should return [[0, 1, 2, 3], [4, 5]].
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); //should return [[0, 1, 2], [3, 4, 5], [6]].
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)); //should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+
+
+}//block
+
+
+
 
--- exercises.js
+++ exercises.js
@@ -199,4 +199,20 @@ console.log(titleCase('sHoRt AnD sToUt')); // Short And Stout
   }//block
 
 //EXERCISE #16
- //. 
+  //.Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+  //.Our goal for this Algorithm is to split arr (first argument) into smaller chunks of arrays with the length provided by size (second argument). There are 4 green checks (objectives) our code needs to pass in order to complete this Algorithm:
+  //? bare in mind slice method
+
+{
+   function chunkArrayInGroups(arr, size) {
+      return 'original';
+    }
+
+    //test
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)); //should return [[0, 1], [2, 3], [4, 5]].
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)); //should return [[0, 1, 2, 3], [4, 5]].
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); //should return [[0, 1, 2], [3, 4, 5], [6]].
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)); //should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+
+
+}//block
