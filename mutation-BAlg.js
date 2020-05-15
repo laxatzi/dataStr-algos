cb878992fd5168af5abc9af3e30435ba303fabc4
laxatzi <laxatzi@yahoo.com>
2020-05-15 16:55:57 +0300

    code in mutation-BAlg.js resolving mutation algo

---
+++ mutation-BAlg.js
@@ -0,0 +1,29 @@
+
+
+//EXERCISE #15
+  //.Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+  //.For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+  //.The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+  //.Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+  //. Θέλω τα γράμματα που υπάρχουν στο δεύτερο element να υπάρχουν και στο πρώτο. Οχι το αντιθετο.
+  //. check JavaScript String.prototype.indexOf() 
+  {
+      
+
+   function mutation(arr) {
+     return arr.map(el => el.toLowerCase().split('').sort().join(''));
+     //.reduce((a,b)=> {
+       if(a === b) return true;
+       return false;
+    // })
+    }
+  
+    //test
+    console.log(mutation(["voodoo", "no"])); //false
+    console.log(mutation(["Mary", "Army"])); //true
+    console.log(mutation(["Mary", "Aarmy"])); //true
+    console.log(mutation(["Alien", "line"])); // true  
+}//block
