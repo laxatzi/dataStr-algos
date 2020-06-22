0a35a8eea820ea7b5a040bb02f9246600e01ce4e
laxatzi <laxatzi@yahoo.com>
2020-06-22 13:02:34 +0300

    update sandbox.js file

--- sandbox.js
+++ sandbox.js
@@ -4,13 +4,25 @@
 //. Params: We are given an array of whole, positive integers (no negative numbers or floats). We are to write //. a function and return the second largest integer.
 {
   function secondLargest(arr){
-    let bothLarger = arr.sort((a,b)=> a-b).slice(-2);
-    return Math.min(...bothLarger);
+    
   };
   console.log(secondLargest([2,1,5,3,45,23,34])); // 34
 }// end block;
 
+//.Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+//.Our goal for this Algorithm is to split arr (first argument) into smaller chunks of arrays with the length provided by size (second argument). There are 4 green checks (objectives) our code needs to pass in order to complete this Algorithm:
+  //? bear in mind slice method
+  //? bear in mind concat method
+  {
+    function chunkArrayInGroups(arr, size) {
+       
+     }
+ 
+     //test
+     console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)); //should return [[0, 1], [2, 3], [4, 5]].
+     console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)); //should return [[0, 1, 2, 3], [4, 5]].
+     console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); //should return [[0, 1, 2], [3, 4, 5], [6]].
+     console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)); //should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
 
 
-
-   
+    }
