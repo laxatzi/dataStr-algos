4f0cd8b513bbe3ccecc78e3e06769e9eab277de5
laxatzi <laxatzi@yahoo.com>
2020-06-23 12:43:02 +0300

    resolve second bigger number algorithm in sandbox.js file

--- sandbox.js
+++ sandbox.js
@@ -4,10 +4,14 @@
 //. Params: We are given an array of whole, positive integers (no negative numbers or floats). We are to write //. a function and return the second largest integer.
 {
   function secondLargest(arr){
-    
+    let sortedOut = arr.sort((a,b)=> (a-b));
+     sortedOut = sortedOut.slice(arr.length - 2);
+    return Math.min(...sortedOut);
   };
   console.log(secondLargest([2,1,5,3,45,23,34])); // 34
 }// end block;
+
+
 
 //.Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 //.Our goal for this Algorithm is to split arr (first argument) into smaller chunks of arrays with the length provided by size (second argument). There are 4 green checks (objectives) our code needs to pass in order to complete this Algorithm:
@@ -28,25 +32,5 @@
     }//end block
 
   
-//Check if a string (first argument, str) ends with the given target string (second argument, target).
-// With the use of substring method
-{
-  function confirmEnding(str, target) {
-        if (str.substring(str.length - target.length) === target) return true;
-        return false;
-     }
-   
-   console.log(confirmEnding("Bastian", "astian")); // true
-  }// end block
-  
-  // use of slice
-  {
-  function confirmEnding(str,target){
-     if(str.slice(str.length-target.length) === target) return true;
-     return false;
-  }
-  console.log(confirmEnding("Lambros", "bos")); // false
-  
-  
-  }//end block
+
 
