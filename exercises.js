7845cef2b26b0f0c7c37d6f1a2ce68689c1ab9d7
laxatzi <laxatzi@yahoo.com>
2020-06-24 15:25:32 +0300

    create removeDups.js file and resolve respective algo in exercises.js file

--- exercises.js
+++ exercises.js
@@ -244,3 +244,35 @@ console.log(sumAll([10, 5])); // should return 45.
   function secondLargest(arr){};
 
 }// end block;
+
+// EXERCISE #19
+//. Q: Remove Duplicate Elements from an Array
+{
+  let cities = ["Athens","Athens", "Thessaloniki", "Iraklion", "Drama", "Thessaloniki"];
+
+//Remove Dups from an Array
+ 
+function removeDups(arr){
+    return arr.sort().reduce(function(acc ,curr){
+    if(curr !== acc[acc.length-1]) acc.push(curr);
+      // OR curr !== acc[0] acc.unshift(curr) -- I compare either from left to right or vice versa
+    return acc;
+  }, []);
+
+}
+let cities = ["Athens","Athens", "Thessaloniki", "Iraklion", "Drama", "Thessaloniki"];
+
+//Remove Dups from an Array
+ 
+function removeDuplicates(arr){
+    return arr.sort().reduce(function(acc ,curr){
+    if(curr !== acc[acc.length-1]) acc.push(curr);
+      // OR (curr !== acc[0]) acc.unshift(curr) -- Compare either from left to right or vice versa
+    return acc;
+  }, []);
+
+}//end function
+    console.log(cities.sort());
+    console.log(removeDuplicates(cities)); // ["Athens", "Drama", "Iraklion", "Thessaloniki"]
+
+}// end block
