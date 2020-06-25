901a3433e274e74cdf696a60d805d38c1fddb971
laxatzi <laxatzi@yahoo.com>
2020-06-25 12:11:27 +0300

    update sandbox.js file

--- sandbox.js
+++ sandbox.js
@@ -5,7 +5,8 @@
   //? bear in mind concat method
   {
     function chunkArray(arr, size) {
-      
+      if(arr.length <= size) return [arr];
+      return [arr.slice(0,size)].concat([chunkArray(arr.slice(size), size)]);
      }
  
      //test
