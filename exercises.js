00e1d43b9fa65f233faaf86591601bdb31d42913
laxatzi <laxatzi@yahoo.com>
2020-07-01 15:18:49 +0300

    create seekDestroy.js file

--- exercises.js
+++ exercises.js
@@ -277,3 +277,20 @@ function removeDups(arr){
  console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
   //. should return ["diorite", "pink wool"].
 }//block
+
+// EXERCISE #20
+// Intermediate Algorithm Scripting: Seek and Destroy
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+// Note
+// You have to use the arguments object.
+
+{
+
+   function destroyer(arr) {
+      
+      }
+
+  console.log(destroyer(["tree", "hamburger", 53], "tree", 53)); // should return ["hamburger"]
+
+}//end block
