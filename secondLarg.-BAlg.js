0289fb3e384ba466b2c004f0c3e308fd86203f19
laxatzi <laxatzi@yahoo.com>
2020-07-20 12:27:40 +0300

    second algo to resolve exercise

--- secondLarg.-BAlg.js
+++ secondLarg.-BAlg.js
@@ -1,1 +1,25 @@
+//EXERCISE #18
+//. Q: Find the second largest number in a given array.
+//. Params: We are given an array of whole, positive integers (no negative numbers or floats). We are to write //. a function and return the second largest integer.
+{
+    function secondLargest(arr){
+        let max = Math.max(...arr);
+        let maxi = arr.indexOf(max);
+        arr.splice(maxi, 1);
+        return Math.max(...arr);
+    };
+  
+    console.log(secondLargest([3, 35, 25, 18, 190])); // 35;
+  }// end block;
 
+  // Second algo
+  {
+    function secondLargest(arr){
+        let sorted = arr.sort((a,b)=> a-b);
+        sorted.pop();
+        return Math.max(...sorted);
+        
+      };
+    
+      console.log(secondLargest([1,11, 222, 1034])); // 222
+  }
