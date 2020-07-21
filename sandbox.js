e72071cf78e347b24b7115805f04ac0e05639d28
laxatzi <laxatzi@yahoo.com>
2020-07-21 17:23:54 +0300

    resolve js exercise with the seek and destroy algo

--- sandbox.js
+++ sandbox.js
@@ -41,7 +41,8 @@ function removeDups(arr){
 
 // EXERCISE #21
 // Intermediate Algorithm Scripting: Seek and Destroy
-// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments.
+// Remove all elements from the initial array that are of the same value as these arguments.
 
 // Note
 // You have to use the arguments object.
@@ -49,10 +50,12 @@ function removeDups(arr){
 {
 
    function destroyer(arr) {
-      return something 
+      let args = [...arguments];
+      return args[0].filter(el => !args.includes(el));
+    
       }
 
-//  console.log(destroyer(["tree", "hamburger", 53], "tree", 53)); // should return ["hamburger"]
+  console.log(destroyer(["tree", "hamburger", 53], "tree", 53)); // should return ["hamburger"]
 
 }//end block
 
