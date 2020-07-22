105dc246515287c1c78441f07b32b62a2d8f12ec
laxatzi <laxatzi@yahoo.com>
2020-07-22 10:32:16 +0300

    revise seek&destroy algorithm

--- sandbox.js
+++ sandbox.js
@@ -50,37 +50,17 @@ function removeDups(arr){
 {
 
    function destroyer(arr) {
-      let args = [...arguments];
-      return args[0].filter(el => !args.includes(el));
-    
+     let args = [...arguments];
+     return args[0].filter(el => !args.includes(el));
       }
 
-  console.log(destroyer(["tree", "hamburger", 53], "tree", 53)); // should return ["hamburger"]
+  //console.log(destroyer(["tree", "hamburger", 53], "tree", 53)); // should return ["hamburger"]
 
 }//end block
-
 
 
 
 //. EXERCISE #22
-// Intermediate Algorithm Scripting: Wherefore art thou
-// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
-
-// For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
-
-{
-  function whatIsInAName(collection, source) {
-   let sourceKey = Object.keys(source);
-   
-  }
- 
- // console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })); // should return [{ first: "Tybalt", last: "Capulet" }].
-
-}//end block
-
-
-
-//. EXERCISE #23
 // SumAllNums
 (function () {
   'use strict';
@@ -96,7 +76,7 @@ function sumAll([ a, b ] ) {
 
 })();
 
-//. EXERCISE #24
+//. EXERCISE #23
 // Intermediate Algorithm Scripting: Wherefore art thou
 // Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
 
