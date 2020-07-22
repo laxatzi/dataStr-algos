0db87d65abb370ac06926ecd244e425e668e4000
laxatzi <laxatzi@yahoo.com>
2020-07-22 13:10:00 +0300

    resolving exercise with 'matching objects in array' algorithm

--- matchingObj-AAlg.js
+++ matchingObj-AAlg.js
@@ -5,15 +5,33 @@
 // For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
 
 {
-   function whatIsInAName(collection, source) {
-    let sourceKey = Object.keys(source);
-    return collection.filter(obj => {
+   function whatIsInAName(objectsArray, ref) {
+    let sourceKey = Object.keys(ref); 
+    return objectsArray.filter(obj => {
        return sourceKey.every(key => {
-          return obj[key] === source[key];
-       })
-    })
+          return obj[key] === ref[key];
+       });
+    });
    }
   
    console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })); // should return [{ first: "Tybalt", last: "Capulet" }].
+ 
+ }//end block
+
+
+ {
+   function whatIsInAName(objArray, ref) {
+     let refKeys = Object.keys(ref); // 'last'
+     return  objArray.filter(el => {
+       // both the key and its corresponding value must exist within the object we are filtering through
+       return refKeys.map(key => el.hasOwnProperty(key) && el[key]=== ref[key])
+       // we reduce the mapped Boolean values to a single Boolean that indicates whether all srcKeys pass the conditions checked above.
+       .reduce((a,b)=> a && b);
+     });
+     
+   }
+  
+  console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }],
+   { last: "Capulet" })); // should return [{ first: "Tybalt", last: "Capulet" }].
  
  }//end block
--- sandbox.js
+++ sandbox.js
@@ -26,33 +26,41 @@ function removeDups(arr){
 
 //. For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
 function sumAll([ a, b ] ) {
-  let alterer;
-  a < b ? alterer = -1 : alterer = 1;
-  if (a===b) return b;
-  return b + sumAll([a, b + alterer]);
+  
+  //return;
 }
 
- console.log('sum is: ' + sumAll([5,1])); // 15
- console.log('sum is: ' + sumAll([1,5])); // 15
+ //console.log('sum is: ' + sumAll([5,1])); // 15
+ //console.log('sum is: ' + sumAll([1,5])); // 15
+
 
 })();
 
 //. EXERCISE #23
 // Intermediate Algorithm Scripting: Wherefore art thou
-// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name
-// and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection 
-//if it is to be included in the returned array.
+// Make a function that looks through an array of objects (first argument) and returns an array of 
+// all objects that have matching name and value pairs (second argument).
+// Each name and value pair of the source object has to be present in the object from the collection 
+// if it is to be included in the returned array.
 
 // For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null },
 // { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" },
-// then you must return the third object from the array (the first argument), 
+// then you must return the third object from the array (in the first argument), 
 // because it contains the name and its value, that was passed on as the second argument.
 
 {
-  function whatIsInAName(collection, source) {
- 
+  function whatIsInAName(objArray, ref) {
+    let refKeys = Object.keys(ref); // 'last'
+    return  objArray.filter(el => {
+      // both the key and its corresponding value must exist within the object we are filtering through
+      return refKeys.map(key => el.hasOwnProperty(key) && el[key]=== ref[key])
+      // we reduce the mapped Boolean values to a single Boolean that indicates whether all srcKeys pass the conditions checked above.
+      .reduce((a,b)=> a || b);
+    });
+    
   }
  
- // console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })); // should return [{ first: "Tybalt", last: "Capulet" }].
+ console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }],
+  { last: "Capulet" })); // should return [{ first: "Tybalt", last: "Capulet" }].
 
 }//end block
