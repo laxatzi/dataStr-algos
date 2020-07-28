a70e87aa95dd56647f8d5a97ec6e37dbba7f1c79
laxatzi <laxatzi@yahoo.com>
2020-07-28 11:43:21 +0300

    modify sandbox.js file

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+
+//. EXERCISE #22
+// Spinal case
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+{
+  function spinalCase(str) {
+    return str.split(" ");
+  }
+ 
+console.log(spinalCase("thisIsSpinalTap")); //   return "this-is-spinal-tap".
+}//end block
