893a2c2df759dccfb22cfc2a4af50773bbe33245
laxatzi <laxatzi@yahoo.com>
2020-07-30 18:40:38 +0300

    resolve pigLatin exercise with respective algorithm

--- sandbox.js
+++ sandbox.js
@@ -75,14 +75,12 @@ function removeDups(arr){
 {
 
    function destroyer(arr) {
-    let args = [...arguments];
-    return arr.filter(el => {
-      return !args.includes(el);
-    });
+  
+   //return;
     
   }
 
- console.log(destroyer(["tree", "hamburger", 53], "tree", 53)); // should return ["hamburger"]
+ //console.log(destroyer(["tree", "hamburger", 53], "tree", 53)); // should return ["hamburger"]
 
 }//end block
 
@@ -123,5 +121,26 @@ function removeDups(arr){
   // console.log(spinalCase("this Is Spinal Tap")); //   return "this-is-spinal-tap".
   // console.log(spinalCase("this_Is_Spinal_Tap")); //   return "this-is-spinal-tap".
   // console.log(spinalCase("thisIsSpinalTap")); //   return "this-is-spinal-tap".
+
+}//end block
+
+
+// PIG LATIN
+    // Pig Latin is a way of altering English Words. The rules are as follows:
+
+    // - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add "ay" to it.
+
+    // - If a word begins with a vowel, just add "way" at the end.
+
+{
+
+function pigLatin(str) {
+  let consonantCluster = str.match(/^[^aeiou]+/);
+
+ return consonantCluster !== null ? str.replace(consonantCluster, '').concat(consonantCluster + 'ay') : str.concat('way');
+}
+
+console.log(pigLatin("grreen"));
+console.log(pigLatin('aubergine'));
 
 }//end block
