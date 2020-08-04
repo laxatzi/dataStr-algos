c3635174b9a0576aee3812dfe5e8420325d9d433
laxatzi <laxatzi@yahoo.com>
2020-08-04 15:12:13 +0300

    alternative algorithm to search and replace challenge

--- sandbox.js
+++ sandbox.js
@@ -39,13 +39,31 @@
     {
 
       function myReplace(str, before, after) {
+        
        let regex = /^[A-Z]/;
        if(regex.test(before)) after = after.charAt(0).toUpperCase().concat(after.substring(1));
        else after = after.charAt(0).toLowerCase().concat(after.substring(1));
        return str.replace(before, after);
+      
       }
    console.log(myReplace("His name is Tom", "Tom", "john")); // should return "His name is John".
    console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms")); // should return "Let us get back to more Algorithms".
    console.log(myReplace("What a nice dog!", "dog", "Fox")); // What a nice fox
 
   }//end block
+
+  // Alternative algo (without test() method and regex)
+  {
+
+    function myReplace(str, before, after) {
+      
+      let indexBefore = str.indexOf(before);
+      if(str[indexBefore] === str[indexBefore].toUpperCase()) after = after.charAt(0).toUpperCase().concat(after.substring(1));
+      return str.replace(before, after);
+    
+    }
+ console.log(myReplace("His name is Tom", "Tom", "john")); // should return "His name is John".
+ console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms")); // should return "Let us get back to more Algorithms".
+ console.log(myReplace("What a nice dog!", "dog", "Fox")); // What a nice fox
+
+}//end block
