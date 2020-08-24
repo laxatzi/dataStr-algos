ef340edd9f4a8172a602d9b65778e281bafc8c00
laxatzi <laxatzi@yahoo.com>
2020-08-24 16:51:05 +0300

    ADD: another js challenge finding the largest num in subarrays

--- sandbox.js
+++ sandbox.js
@@ -87,20 +87,19 @@
 
 // #################################################################################
 
-//Longest Word in a String
+//Return Largest Numbers in Arrays
 
 
 
-// Return the length of the longest word in the provided sentence.
+// Return an array consisting of the largest number from each provided sub-array. 
+// For simplicity, the provided array will contain exactly 4 sub-arrays.
 
-// Your response should be a number.
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 
 {
-  function longestWordLength(str){
-    let splitStr = str.split(' ');
-    return splitStr.map(el => {
-      return el.length;
-    }).reduce((acc, cur)=> cur > acc?cur : acc, []);
+  function largestOfFour(arr)  {
+    return arr;
   }
-  console.log(longestWordLength('There is a fox in the alleys')); //6
-}
+  console.log([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]); //[27, 5, 39, 1001]
+  console.log([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]); // [25, 48, 21, -3]
+}// end block
