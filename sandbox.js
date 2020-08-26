5681d41a04cd75b7ce029b1f54836ba971b4cd29
laxatzi <laxatzi@yahoo.com>
2020-08-26 13:45:22 +0300

    TEST: resolve get index to insert argument algorithm

--- sandbox.js
+++ sandbox.js
@@ -138,7 +138,28 @@
         console.log('something');
       }
 
-      console.log(bouncer([false, null, 0, NaN, undefined, ""])); // []
-      console.log(bouncer([null, NaN, 1, 2, undefined])); // [1,2]
+      // console.log(bouncer([false, null, 0, NaN, undefined, ""])); // []
+      // console.log(bouncer([null, NaN, 1, 2, undefined])); // [1,2]
 
     }
+
+
+    // Where do I Belong
+
+    // Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. 
+    // The returned value should be a number.
+    // For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+    // Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is
+    // less than 20 (index 2) and greater than 5 (index 1).
+
+    {
+      function getIndexToIns(arr, num){
+        arr.push(num);
+        let sorted = arr.sort((a,b)=>a-b);
+        return sorted.indexOf(num);
+      }
+      console.log(getIndexToIns([20, 3, 5], 19)); // 2
+      console.log(getIndexToIns([1,2,3,4], 1.5)); // 1
+    }
+
+    //arr.splice(index, 0, item)
