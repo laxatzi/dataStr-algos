927f76e34c23bf4f69f191ffec44ad4ff3f1aa64
laxatzi <laxatzi@yahoo.com>
2020-08-31 17:02:30 +0300

    TEST: resolve pigLatin algorithm challenge

--- sandbox.js
+++ sandbox.js
@@ -9,13 +9,13 @@
 
 {
   function translatePigLatin(str) {
-    return str;
+    return str.replace(/(^[aeiouy])(\w*)/, '$1$2ay').replace(/(^[^aeiouy]+)(\w*)/, '$2$1way');
   }
   
-// test here
-   //  console.log(translatePigLatin("consonant"));  // onsonantcay
-   // console.log(translatePigLatin("grreen"));  // eengrray
-   // console.log(translatePigLatin('aubergine')); // aubergineway
+
+    console.log(translatePigLatin("consonant"));  // onsonantcay
+   console.log(translatePigLatin("grreen"));  // eengrray
+   console.log(translatePigLatin('aubergine')); // aubergineway
 
 
 } // end block
@@ -121,48 +121,5 @@
 
  // ##################################################################################################
 
-  
-  // Wherefore art thouPassed
 
-    // Make a function that looks through an array of objects (first argument) and returns 
-    // an array of all objects that have matching name and value pairs (second argument). 
-    // Each name and value pair of the source object has to be present in the object from the collection 
-    // if it is to be included in the returned array.
-
-    // For example, if the first argument is [{ first: "Romeo", last: "Montague" }, 
-    // { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, 
-    // then you must return the third object from the array (the first argument),
-    // because it contains the name and its value, that was passed on as the second argument.
-
-    {
-      function whatIsInAName(collection, source){
-        //HINT the source key is the 'key' to the solution
-        return source;
-        }
-      
-        //TEST
-          console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null },
-          { first: "Tybalt", last: "Capulet" },{first: "Lambros"}] // end of collection
-          , { last: "Capulet" }));  //[{ first: "Tybalt", last: "Capulet" }]
-
-          console.log(whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3})); // []
-    }
-  
-
-    //Spinal Tap Case
-
-// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
-
-{
-  function spinalCase(str){
-   return str;
-  }
-
-  console.log(spinalCase("The_Andy_Griffith_Show")); //"this-is-spinal-tap"
-  console.log(spinalCase("The Andy Griffith Show")); // //"this-is-spinal-tap"
-  console.log(spinalCase("theAndyGriffithShow")); // "this-is-spinal-tap"
-  console.log(spinalCase("TheAndy-Griffith_Show"))
-}
-
-//###########################################################################################################
 
