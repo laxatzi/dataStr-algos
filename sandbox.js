2e9398d52c402f820353b055611160bb58625503
laxatzi <laxatzi@yahoo.com>
2020-09-04 16:00:31 +0300

    TEST: revise algorithmic challenge solution for 'missing letter' challeng

--- sandbox.js
+++ sandbox.js
@@ -109,7 +109,14 @@
   // return missing
   {
     function fearNotLetter(str) {
-      
+      let curLetter = str.charCodeAt(0); //97
+      let missing;
+      str.split('').map((el, index)=> {
+        if(str.charCodeAt(index) === curLetter) ++curLetter;
+        else missing =  String.fromCharCode(curLetter);
+      })
+      if(missing === undefined) return "No letter is missing!";
+      return `The missing letter is ${missing}`;
     }
     
     console.log(fearNotLetter("abce")); // "d"
@@ -118,30 +125,3 @@
 
   
     
-
-
-// The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
-
-// Base pairs are a pair of AT and CG. Match the missing element to the provided character.
-
-// Return the provided character as the first element in each array.
-
-// For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
-
-// The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
-
-{
-  function pairElems(str){
-    let dnaObject = {
-      A:'T',
-      T:'A',
-      C:'G',
-      G:"C"
-    }
-    
-
-    return str.split('').map(key => [key, dnaObject[key]]);
-  }
-
-  console.log(pairElems("TTGAG")); // [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]]
-}
