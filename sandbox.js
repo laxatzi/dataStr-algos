e87df8cdf7fbce69593b2a704670ac85df122738
laxatzi <laxatzi@yahoo.com>
2020-09-07 14:04:59 +0300

    TEST: 'unite unique' algorithm challenge and review older algo challenges

--- sandbox.js
+++ sandbox.js
@@ -1,36 +1,5 @@
     
-// #Mutations
 
-  // Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
-  // For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
-  // The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
-  // Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
-
-  {
-    function mutation(arr){
-      return arr;
-    }
-    //Test
-      // console.log(mutation(["hello", "hey"])); // false
-      // console.log(mutation(["floor", "for"])); // true
-      // console.log(mutation(["hello", "Hello"])); // true
-  }
-
-
-// Chunky MonkeyPassed
-
-  // Write a function that splits an array (first argument) into groups the length of size (second argument) 
-  // and returns them as a two-dimensional array.
-
-{
-  function chunkArrayInGroups(arr, size){
-   
-    return arr;
-  }
-
-  //  console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5,6,7,8,9], 3));  //[[0, 1, 2], [3, 4, 5]]
-  //  console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));  //[[0, 1, 2, 3], [4, 5]]
- }
 
  // ##################################################################################################
 
@@ -65,10 +34,14 @@
 
 {
   function uniteUnique(arr){
-    
+    let args = [...arguments];
+    let mergedArray = args.reduce((acc, cur)=> {
+      return acc.concat(cur.filter(el=> acc.indexOf(el) === -1));
+    }, []);
+    return mergedArray;
   }
     
-    console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])); //
+    console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])); // [1,3,2,5,4]
 
   }
 
@@ -76,7 +49,61 @@
 
 //#################################################################################
 
-  
+// SORTED UNION
 
-  
-    
+  // Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+  // In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+  // The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+  // Check the assertion tests for examples.
+
+
+{
+    function uniteUnique(arr){
+     
+    }
+      
+      console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])); // [1,3,2,5,4]
+
+}
+
+
+
+ // #Mutations
+
+    // Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+    // For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+    // The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+    // Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+  {
+    function mutation(arr){
+      return arr[1].toLowerCase().split('').map(el => arr[0].toLowerCase().indexOf(el) !== -1).reduce((a,b)=>a&&b);
+    }
+    //Test
+       console.log(mutation(["hello", "hey"])); // false
+       console.log(mutation(["Floor", "for"])); // true
+       console.log(mutation(["hello", "Hello"])); // true
+  }
+
+// Chunky Monkey
+
+  // Write a function that splits an array (first argument) into groups the length of size (second argument) 
+  // and returns them as a two-dimensional array.
+
+{
+  function chunkArrayInGroups(arr, size){
+    if(arr.length <= size) return [arr];
+    return [arr.slice(0,size)].concat(chunkArrayInGroups(arr.slice(size), size));
+  }
+
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5,6,7,8,9], 3));  //[[0, 1, 2], [3, 4, 5]]
+    console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));  //[[0, 1, 2, 3], [4, 5]]
+ }
+
+
+
+
+
