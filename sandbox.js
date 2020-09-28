eb9ce61e7f53a62a04ed80f93b653243b2aaa25d
laxatzi <laxatzi@yahoo.com>
2020-09-28 11:35:58 +0300

    INITIAL: set 'smallest common' exercise on sandbox.js file

--- sandbox.js
+++ sandbox.js
@@ -241,6 +241,41 @@ console.log('Sorted Union below');
 }
 
 
+{
+  console.log('Smallest Common Multiple');
+// Find the smallest common multiple on the provided params that can be evenly divided by both, as well as
+// all the sequential numbers in the range between these two params. 
+// The range is an array of two numbers, not necessarily in numerical order.
+
+// For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly 
+// divisible by all numbers between 1 and 3. The answer here would be 6.
+
+// NOTES
+  // ...
+
+
+
+    function leastCommon(arr) {
+  // Range between arr elements
+      
+  // The Math   
+    // Euclidean algorithm for Greatest Common divisor 
+        const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+    // Least Common Multiple for two numbers based on Euclidean algorithm
+        const lcm = (a,b)=> (a * b)/ gcd(a, b);
+
+  // Initially the solution is assigned to the highest value of the array
+
+  // implement the Euclidean algorithm to each array's element
+    
+      
+}
+    console.log(leastCommon([1, 5])); // 60
+    console.log(leastCommon([5, 1])); // 60
+    console.log(leastCommon([2, 10])); //2520
+}
+
+
 
 
 // Intermediate Algorithm Scripting: Sum All Primes
