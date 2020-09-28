c5cf00b9e4a95f965fa9b1e386b95e085f5a5019
laxatzi <laxatzi@yahoo.com>
2020-09-28 15:10:47 +0300

    ADD: js snippet for creating range of numbers from an array

--- sandbox.js
+++ sandbox.js
@@ -241,41 +241,6 @@ console.log('Sorted Union below');
 }
 
 
-{
-  console.log('Smallest Common Multiple');
-// Find the smallest common multiple on the provided params that can be evenly divided by both, as well as
-// all the sequential numbers in the range between these two params. 
-// The range is an array of two numbers, not necessarily in numerical order.
-
-// For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly 
-// divisible by all numbers between 1 and 3. The answer here would be 6.
-
-// NOTES
-  // ...
-
-
-
-    function leastCommon(arr) {
-  // Range between arr elements
-      
-  // The Math   
-    // Euclidean algorithm for Greatest Common divisor 
-        const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
-    // Least Common Multiple for two numbers based on Euclidean algorithm
-        const lcm = (a,b)=> (a * b)/ gcd(a, b);
-
-  // Initially the solution is assigned to the highest value of the array
-
-  // implement the Euclidean algorithm to each array's element
-    
-      
-}
-    console.log(leastCommon([1, 5])); // 60
-    console.log(leastCommon([5, 1])); // 60
-    console.log(leastCommon([2, 10])); //2520
-}
-
-
 
 
 // Intermediate Algorithm Scripting: Sum All Primes
@@ -306,5 +271,42 @@ console.log('Sorted Union below');
   }
   console.log("Check primality!");
   console.log(sumPrimes(11)); // 28    
+
+}
+
+
+{
+
+function leastCommon(arr) {
+  // Range between arr elements
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let range = Array(max-min+1).fill().map((v,i)=> i + min);
+      return range;
+  // The Math   
+    // Euclidean algorithm for Greatest Common divisor 
+
+        const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+
+    // Least Common Multiple for two numbers based on Euclidean algorithm
+
+        const lcm = (a,b)=> (a * b)/ gcd(a, b);
+
+  // Initially the solution is assigned to the highest value of the array
+
+      let currentLcm = max; 
+
+  // implement the Euclidean algorithm to each array's element
+
+      while(min<max){
+        currentLcm = lcm(currentLcm, min); 
+        min++; 
+      }
+
+      return currentLcm;
+}
+    console.log(leastCommon([1, 5])); // 60
+    console.log(leastCommon([5, 1])); // 60
+    console.log(leastCommon([2, 10])); //2520
 
 }
--- smallestCM.js
+++ smallestCM.js
@@ -6,28 +6,29 @@ console.log('Smallest Common Multiple');
 // For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly 
 // divisible by all numbers between 1 and 3. The answer here would be 6.
 
-// NOTES
-  // ...
-
 
 {
     function leastCommon(arr) {
   // Range between arr elements
-      let max = Math.max(...arr);
-      let min = Math.min(...arr);
-      for (var i = min; i <= max; i++) {
-        [min, max].unshift(i);
-      }
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let range = Array(max-min+1).fill().map((v,i)=> i + min);
+      
   // The Math   
     // Euclidean algorithm for Greatest Common divisor 
+
         const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+
     // Least Common Multiple for two numbers based on Euclidean algorithm
+
         const lcm = (a,b)=> (a * b)/ gcd(a, b);
 
   // Initially the solution is assigned to the highest value of the array
+
       let currentLcm = max; 
 
   // implement the Euclidean algorithm to each array's element
+
       while(min<max){
         currentLcm = lcm(currentLcm, min); 
         min++; 
