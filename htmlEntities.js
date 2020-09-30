9653e9c3da86fb8c8efdcafd224241d8b2407256
laxatzi <laxatzi@yahoo.com>
2020-09-30 10:54:56 +0300

    INIT: create file for 'sum fibonacci odd numbers' algorithm

--- htmlEntities.js
+++ htmlEntities.js
@@ -1,1 +1,15 @@
+//Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
+// <:&lt; >:&gt; &:&amp; ":&quot; ':&apos;
+
+{
+
+    function htmlEntities(str){
+        return str;
+    }
+ 
+
+console.log(convertHTML("Dolce & Gabbana"));
+
+
+}//end block
