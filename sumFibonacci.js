070bc68666e2e48a984d05d9336126433346e2ef
laxatzi <laxatzi@yahoo.com>
2020-09-30 16:32:39 +0300

    TEST: solve 'sum of fibonacci numbers in range' algorithmic exercise

--- sumFibonacci.js
+++ sumFibonacci.js
@@ -10,11 +10,53 @@
     {
 
         function sumFibs(num) {
-          return num;
+
+        // "given a positive integer "num" check condition
+            if(num<=0) return false;
+
+        // represent  fibonacci sequence in our code
+            let fibonacciSequence = [1,1];
+            let i;
+            while((i = fibonacciSequence[0] + fibonacciSequence[1]) <= num){ // fibonacci axiom
+                fibonacciSequence.unshift(i);
+            }
+        
+        
+        // filter odd fibonacci numbers and then sum them up
+            return fibonacciSequence.filter(el => el%2!==0).reduce((a,b)=> a + b );
           
         }
   
-        console.log(sumFibs(1000)); //1785
+        console.log(sumFibs(10)); //10
+        console.log(sumFibs(100)); // 188
         console.log(sumFibs(75024)); // 60696
-        console.log(sumFibs(75025)); // 135721
-      }
+       
+ } // end block
+
+ console.log("FIND SUM OF FIBONACCI NUMBERS IN RANGE");
+ // Fibonacci numbers in range
+ {
+
+    function fibsInRangeSum(arr){
+     // range
+     let max = Math.max(...arr);
+     let min = Math.min(...arr);
+     let range = Array(max-min+1).fill().map((_,i)=> i + 1);
+     // find fibonacci
+     function fibonacciNumbs(num) {
+        if(num<=0) return false;
+        let fibSequence = [1,1];
+        let i;
+        while((i = fibSequence[0] + fibSequence[1]) <= num){
+         fibSequence.unshift(i);
+        }
+        return fibSequence;
+      } // end fibonacci numbs
+
+      return fibonacciNumbs(max).filter(el => el>=min).reduce((a,b)=> a + b);
+
+    } // end function
+console.log(fibsInRangeSum([3,10])); // 16
+console.log(fibsInRangeSum([14,100])); // 199
+
+ }// end block
