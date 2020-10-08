b4df3bd597926e1681964ed44fbe9a4604bdb7c9
laxatzi <laxatzi@yahoo.com>
2020-10-08 12:41:34 +0300

    TEST: solve js challeng unnesting deep level arrays

--- steamRoller.js
+++ steamRoller.js
@@ -1,1 +1,15 @@
+// STEAM ROLLER
+
+//Flatten a nested array. You must account for varying levels of nesting.
+
+    function steamrollArray(arr){
+       
+        // we are gonna use recursion reducing the array => if element is number will be shifted to new array,
+        // else recursion will go one level deep
+        return arr.reduce((acc, val)=> acc.concat(Array.isArray(val)?steamrollArray(val):val), []);
+    }
+      
+
+  console.log(steamrollArray([1, [], [3, [[4]]]])); // [1, 3, 4];
+  console.log(steamrollArray([1, {}, [3, [[4]]]])); // [1, {}, 3, 4];
 
