0e7b59bb7b16f64eb9a69460db4b76033490f961
laxatzi <laxatzi@yahoo.com>
2020-11-23 11:42:48 +0200

    ADD: nullish coalescing operator in conditional

--- sandboxPark.js
+++ sandboxPark.js
@@ -3,16 +3,24 @@
 
 let sumFunctionThatMayBreak = (a, b, inconsistentParameter) => a+b+inconsistentParameter
 
- console.log(sumFunctionThatMayBreak(12,40, undefined)); // => NaN
+ console.log(sumFunctionThatMayBreak(12,40)); // => NaN
 
 }//end block
 
 {
-	let sumFunctionThatMayBreak = (a,b, inconsistentParameter)=> a + b (!!inconsistentParameter?inconsistentParameter:0);
+	console.log('DISMISS ALL FAULTY')
+	let sumFunctionThatMayBreak = (a,b, inconsistentParameter)=>a+b+(!!inconsistentParameter?inconsistentParameter:0);
+	console.log(sumFunctionThatMayBreak(12,40, null)); // => 52
+	console.log(sumFunctionThatMayBreak(12,40, '')); // => 52
+
 }
 {
-	let sumFunctionThatMayBreak = (a,b,inconsistentParameter)=>  a+b+(!!inconsistentParameter??inconsistentParameter);
+	console.log('DISMISS ONLY NULL AND UNDEFINED')
+
+	let sumFunctionThatMayBreak = (a,b,inconsistentParameter)=>  a+b+(inconsistentParameter??0);
+	console.log(sumFunctionThatMayBreak(12,40,'')); // => "52"
 	console.log(sumFunctionThatMayBreak(12,40, null)); // => 52
+
 
 }
 
