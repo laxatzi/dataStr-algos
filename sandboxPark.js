047e9641ed7c974ae112b27c5b831ade16e9fb7b
laxatzi <laxatzi@yahoo.com>
2020-11-23 11:09:20 +0200

    REFACTOR: if/else statements case studies

--- sandboxPark.js
+++ sandboxPark.js
@@ -1,2 +1,19 @@
 
-  
+{
+
+let sumFunctionThatMayBreak = (a, b, inconsistentParameter) => a+b+inconsistentParameter
+
+ console.log(sumFunctionThatMayBreak(12,40, undefined)); // => NaN
+
+}//end block
+
+{
+	let sumFunctionThatMayBreak = (a,b, inconsistentParameter)=> a + b (!!inconsistentParameter?inconsistentParameter:0);
+}
+{
+	let sumFunctionThatMayBreak = (a,b,inconsistentParameter)=>  a+b+(!!inconsistentParameter??inconsistentParameter);
+	console.log(sumFunctionThatMayBreak(12,40, null)); // => 52
+
+}
+
+
