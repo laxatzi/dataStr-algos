a913bfc6f59358d0188d2b422ac2ce9f663078d5
laxatzi <laxatzi@yahoo.com>
2021-02-03 16:31:08 +0200

    TEST: resolve exercise on checking object keys

--- sandboxPark.js
+++ sandboxPark.js
@@ -21,5 +21,40 @@
 			],
 			3
 		)
-	); // should return []
+	); // should return [19, 5, 9]
 } //end block
+//EXERCISE #4
+//Check objects' property
+
+// We've created an object, users, with some users in it and a function isEveryoneHere,
+//which we pass the users object to as an argument. Finish writing this function so that it returns true
+//only if the users object contains all four names, Alan, Jeff, Sarah, and Ryan, as keys, and false otherwise.
+
+{
+	let users = {
+		Alan: {
+			age: 27,
+			online: true,
+		},
+		Jeff: {
+			age: 32,
+			online: true,
+		},
+		Sarah: {
+			age: 48,
+			online: true,
+		},
+		Ryan: {
+			age: 19,
+			online: true,
+		},
+	};
+
+	function isEveryoneHere(obj) {
+		if ("Alan" in obj && "Jeff" in obj && "Sarah" in obj && "Ryan" in obj)
+			return true;
+		else return false;
+	}
+
+	console.log(isEveryoneHere(users));
+} // end block
