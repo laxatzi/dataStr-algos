173f33a4f71eb0219991bb16b4d6c9ade63dd94c
laxatzi <laxatzi@yahoo.com>
2021-02-04 12:46:37 +0200

    TEST: itterate through object exercise

--- sandboxPark.js
+++ sandboxPark.js
@@ -58,3 +58,38 @@
 
 	console.log(isEveryoneHere(users));
 } // end block
+
+//EXERCISE #5
+//Iterate Through the Keys of an Object with a for...in Statement
+
+//We've defined a function countOnline which accepts one argument (a users object).
+// Use a for...in statement within this function to loop through the users object passed into the function
+//and return the number of users whose online property is set to true.
+// An example of a users object which could be passed to countOnline is shown below.
+//Each user will have an online property with either a true or false value.
+
+{
+	const usersObj = {
+		Alan: {
+			online: true,
+		},
+		Jeff: {
+			online: true,
+		},
+		Sarah: {
+			online: true,
+		},
+	};
+
+	function countOnline(obj) {
+		let numberOfTruths = 0;
+		for (let property in obj) {
+			if (obj[property].online === true) numberOfTruths += 1;
+		}
+
+		return numberOfTruths;
+	}
+
+	console.log(countOnline(usersObj));
+}
+// end block
