490b00c7fee396d66e3b4bfdff923a879347abf8
laxatzi <laxatzi@yahoo.com>
2021-02-17 13:08:07 +0200

    TEST: copy each element of an arr in another arr, in index n, and in order

--- sandboxPark.js
+++ sandboxPark.js
@@ -87,3 +87,20 @@ console.log(factorialize(0));//1
 
 	console.log(titleCase("sHoRt AnD sToUt")); // Short And Stout
 } // block
+
+//EXERCISE #19
+// Slice and SplicePassed
+
+//. You are given two arrays and an index.
+//. Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+//. Begin inserting elements at index n of the second array. Return the resulting array.
+//The input arrays should remain the same after the function runs.
+
+{
+	function frankenSplice(arr1, arr2, n) {
+		return "Copy each element of the an array into another one, at index n, and in order."
+	}
+
+	console.log(frankenSplice([1, 2, 3], [4, 5], 1)); // [4, 1, 2, 3, 5]
+} // end block
+
--- solutions.js
+++ solutions.js
@@ -92,3 +92,25 @@
 
 	console.log(titleCase("sHoRt AnD sToUt")); // Short And Stout
 } // block
+
+
+//EXERCISE #19
+// Slice and SplicePassed
+
+//. You are given two arrays and an index.
+//. Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+//. Begin inserting elements at index n of the second array. Return the resulting array.
+//The input arrays should remain the same after the function runs.
+
+{
+	function frankenSplice(arr1, arr2, n) {
+    // copy the array coz splice cause mutation
+		const arr2Copy = arr2.slice(); 
+    // mutate the array BUT NOT return it - splice returns the spliced element (if not such thing, an empty array)
+	    arr2Copy.splice(n,0,...arr1);
+    // return the already mutated array
+		return arr2Copy;
+	}
+
+	console.log(frankenSplice([1, 2, 3], [4, 5], 1)); // [4, 1, 2, 3, 5]
+} // end bl
