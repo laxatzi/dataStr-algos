6b658b0a331920255434c9d357841fa8d033a313
laxatzi <laxatzi@yahoo.com>
2021-03-01 12:42:48 +0200

    TEST: solve 'test and replace' algorithm

--- sandboxPark.js
+++ sandboxPark.js
@@ -422,10 +422,7 @@ console.log(factorialize(0));//1
 // Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
 {
 	function pigLatin(str) {
-		const vowel = /[aeiou]/;
-		const consonantCluster = /^[^aeiou]+/g;
-		if(str[0].match(vowel)) return str + "way";
-		return str.replace(consonantCluster, '').concat(str.match(consonantCluster),"ay");
+		return "pig latin algorithm";
 	}
 
 
@@ -434,3 +431,29 @@ console.log(factorialize(0));//1
 	console.log(pigLatin("paragraphs")); // should return "aragraphspay"
 	console.log(pigLatin("algorithm")); // algorithmway
 } // end block
+
+//EXERCISE #29
+// Search and ReplacePassed
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+//First argument is the sentence to perform the search and replace on.
+//Second argument is the word that you will be replacing (before).
+//Third argument is what you will be replacing the second argument with (after).
+//Note
+//Preserve the case of the first character in the original word when you are replacing it.
+//For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+
+{
+	function myReplace(str, before, after) {
+	  if (before[0].match(/[A-Z]/g)){
+		  after = after.replace(after[0], after[0].toUpperCase());
+		  return str.replace(before, after);
+	  }
+		  after = after.replace(after[0], after[0].toLowerCase());
+		  return str.replace(before, after);
+	  
+	 
+	}
+
+	console.log( myReplace("He is Sleeping on the couch", "Sleeping", "sitting")); //"He is Sitting on the couch"
+	console.log(myReplace("I think we should look up there", "up", "Down")); // "I think we should look down there"
+} //end block
--- solutions.js
+++ solutions.js
@@ -350,7 +350,7 @@
 } //end block
 
 
-//EXERCISE #27
+//EXERCISE #20
 //Spinal Tap Case
 //Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
@@ -367,7 +367,7 @@
 } //end block
 
 
-//EXERCISE #28
+//EXERCISE #21
 //// Pig Latin
 // Pig Latin is a way of altering English Words. The rules are as follows:
 
@@ -390,3 +390,29 @@
 	console.log(pigLatin("paragraphs")); // should return "aragraphspay"
 	console.log(pigLatin("algorithm")); // algorithmway
 } // end block
+
+
+//EXERCISE #22
+// Search and ReplacePassed
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+//First argument is the sentence to perform the search and replace on.
+//Second argument is the word that you will be replacing (before).
+//Third argument is what you will be replacing the second argument with (after).
+//Note
+//Preserve the case of the first character in the original word when you are replacing it.
+//For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+
+{
+	function myReplace(str, before, after) {
+	  if (before[0].match(/[A-Z]/g)){
+		  after = after.replace(after[0], after[0].toUpperCase());
+		  return str.replace(before, after);
+	  }
+		  after = after.replace(after[0], after[0].toLowerCase());
+		  return str.replace(before, after);
+	  
+	}
+
+	console.log( myReplace("He is Sleeping on the couch", "Sleeping", "sitting")); //"He is Sitting on the couch"
+	console.log(myReplace("I think we should look up there", "up", "Down")); // "I think we should look down there"
+} //end block
