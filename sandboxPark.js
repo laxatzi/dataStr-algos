4d285acc73b2030d04aa31e241a7405e476a462f
laxatzi <laxatzi@yahoo.com>
2021-04-08 11:51:48 +0300

    TEST: solve 'add together' algorithm

--- sandboxPark.js
+++ sandboxPark.js
@@ -253,10 +253,6 @@ console.log('YOU ARE HERE!');
 {
 
 	function mutation(arr) {
-		const secondElem = arr[1].toLowerCase();
-		const firstElem = arr[0].toLowerCase();
-		
-     return secondElem.split('').every((el) => firstElem.indexOf(el) !== -1);
 	    return "solve mutation algorithm";
 	  }
 	  
@@ -717,8 +713,8 @@ console.log('YOU ARE HERE!');
 // EXERCISE #34
 //Arguments Optional
 
-   // Create a function that sums two arguments together. If only one argument is provided, then return a function that expects 
-   // one argument and returns the sum.
+   // Create a function that sums two arguments together. If only one argument is provided, then return 
+   // a function that expects one argument and returns the sum.
    // For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
    // Calling this returned function with a single argument will then return the sum:
    // var sumTwoAnd = addTogether(2);
@@ -729,21 +725,40 @@ console.log('YOU ARE HERE!');
    // we have to create a program that does the following:
 
    // It has to add two numbers passed as parameters and return the sum.
-   // It has to check if any of the numbers are actual numbers, otherwise return undefined and stop the program right there.
+   // It has to check if any of the numbers are actual numbers, otherwise return undefined 
+   // and stop the program right there.
    // It has to check if it has one or two arguments passed. More are ignored.
-   // If it has only one argument then it has to return a function that uses that number and expects another one, to then add it.
+   // If it has only one argument then it has to return a function that uses that number and expects another one, 
+   // to then add it.
 
 {
 	function addTogether() {
-
-	return 'add together';
+    // ignore more than two args
+	const [arg1, arg2, ] = arguments;
+	// check number type
+	const isInt = function isInt(n) {
+		return Number.isInteger(n);
+	}
+	if (isInt(arg1)) {
+		if (!arg2){
+			return function(arg2) {
+				if (isInt(arg2)){
+					return arg2 + arg1;
+				}
+			}
+		} 
+		if (isInt(arg2)){
+			return arg1 + arg2;
+		}
+	}
+	//return 'add together';
 }// end addTogether
 
 	console.log(addTogether(2,3)); // 5
-   //console.log(addTogether(5)(2)); // 7
+    console.log(addTogether(5)(2)); // 7
 	console.log(addTogether(2,'3')); // undefined
     console.log(addTogether(2,3,4)); // 5
-   // console.log(addTogether(2)([3]));
+    console.log(addTogether(2)([3]));
 }//block
 
 //const curried = x => y => x + y
